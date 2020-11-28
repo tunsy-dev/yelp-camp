73eeb3a9f17908f88951aa180d243023ceb8dea9
Thomas Unsworth <Tom@Thomass-MacBook-Pro.local>
2020-11-28 13:40:08 +0000

    start script added

--- app.js
+++ app.js
@@ -164,6 +164,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
