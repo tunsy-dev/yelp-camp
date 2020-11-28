f407372790a5cdcb1aaa49b1deb1ba81df7a94be
Thomas Unsworth <Tom@Thomass-MacBook-Pro.local>
2020-11-28 14:05:46 +0000

    port correction

--- app.js
+++ app.js
@@ -165,7 +165,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 })
